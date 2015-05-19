7330ce7dfc6dfa382a8368f555fb039ecc7ae8eb
Mitchell <MitchellBinning@gmail.com>
2015-05-19 11:34:37 -0700

    Added title and listview to invite contacts activity

--- FindYou/app/src/main/java/group/csm117/findyou/InviteContactsActivity.java
+++ FindYou/app/src/main/java/group/csm117/findyou/InviteContactsActivity.java
@@ -1,17 +1,53 @@
 package group.csm117.findyou;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class InviteContactsActivity extends ActionBarActivity {
+public class InviteContactsActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    ListView list;
+    private List<String> List_file;
+
+    private void CreateListView()
+    {
+        List_file.add("Coderzheaven");
+        List_file.add("Google");
+        List_file.add("Android");
+        List_file.add("iPhone");
+        List_file.add("Apple");
+        //Create an adapter for the listView and add the ArrayList to the adapter.
+        list.setAdapter(new ArrayAdapter<String>(InviteContactsActivity.this, android.R.layout.simple_list_item_1,List_file));
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3)
+            {
+                //args2 is the listViews Selected index
+                // Send Invite
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite_contacts);
+
+        List_file = new ArrayList<String>();
+        list = (ListView)findViewById(R.id.listview);
+
+        CreateListView();
     }
 
     @Override
