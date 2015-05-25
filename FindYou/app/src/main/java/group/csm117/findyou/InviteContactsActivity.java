4c72c49d930d92ff62bcc230d5f51423b965a182
Mitchell <MitchellBinning@gmail.com>
2015-05-24 22:09:47 -0700

    added Facebook invite friends and invite friend users to event

--- FindYou/app/src/main/java/group/csm117/findyou/InviteContactsActivity.java
+++ FindYou/app/src/main/java/group/csm117/findyou/InviteContactsActivity.java
@@ -1,119 +1,47 @@
 package group.csm117.findyou;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.Button;
 
-import com.facebook.AccessToken;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.facebook.share.model.AppInviteContent;
+import com.facebook.share.widget.AppInviteDialog;
 
 
-public class InviteContactsActivity extends Activity implements AbsListView.OnScrollListener {
+public class InviteContactsActivity extends ActionBarActivity {
 
-    List user_friends;
-    ListView lvFriends;
-    User_friendListAdapterWithCache adapterFriends;
-
-    private boolean lvBusy = false;
-
-    /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite_contacts);
 
-        // populate data
-        user_friends = new ArrayList();
-        GraphRequest request = GraphRequest.newMyFriendsRequest(
-                AccessToken.getCurrentAccessToken(),
-                new GraphRequest.GraphJSONArrayCallback() {
-                    @Override
-                    public void onCompleted(
-                            JSONArray rows,
-                            GraphResponse response) {
-                        // Application code for users friends
-                        try {
-                            Log.d("MyApp", "attemping to grab friends");
-                            Log.d("MyAPP", "number of rows: " + rows.length());
-                            for(int i=0; i<rows.length(); i++){
+        String appLinkUrl, previewImageUrl;
 
-                                Log.d("MyApp", "Inside loop ");
-                                JSONObject e = rows.getJSONObject(i);
+        appLinkUrl = "https://fb.me/517655461722207";
+        previewImageUrl = "http://i378.photobucket.com/albums/oo224/BaconLord/FindYou_Icon.png?t=1432441586";
 
-                                user_friends.add(new User_friend(e.optString("name"), e.getJSONObject("picture").getJSONObject("data").getString("url")));
+        if (AppInviteDialog.canShow()) {
+            AppInviteContent content = new AppInviteContent.Builder()
+                    .setApplinkUrl(appLinkUrl)
+                    .setPreviewImageUrl(previewImageUrl)
+                    .build();
+            AppInviteDialog.show(this, content);
+        }
 
-                            }
-                        } catch (JSONException e) {
-                            Log.e("Error", "json " + e.toString());
-                        }
-                    }
-                });
+        // Set up the submit button click handler
+        Button mActionButton = (Button) findViewById(R.id.continue_button);
+        mActionButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
 
-        Bundle parameters = new Bundle();
-        parameters.putString("fields", "picture.type(small),id,name,link");
-        request.setParameters(parameters);
-        request.executeAsync();
-
-        // Populate list view with more to demonstrate scrolling because I only have 1 friend....
-        user_friends.add(new User_friend("Orange", "http://farm5.staticflickr.com/4142/4787427683_3672f1db9a_s.jpg"));
-        user_friends.add(new User_friend("Apple", "http://farm4.staticflickr.com/3139/2780642603_8d2c90e364_s.jpg"));
-        user_friends.add(new User_friend("Pineapple", "http://farm2.staticflickr.com/1008/1420343003_13eeb0f9f3_s.jpg"));
-        user_friends.add(new User_friend("Grape", "http://4.bp.blogspot.com/-M6qZctUebsU/VKQTpu_ARxI/AAAAAAAAAZQ/Wchoe_Jd1C0/s1600/grape%2B1.JPG"));
-        user_friends.add(new User_friend("Cherry", "http://graphics8.nytimes.com/newsgraphics/2014/06/16/bittman-eat-cherry/ed5c4f4c098cd142650d7c00014e71abf85d2f86/eatopener_cherry.jpg"));
-        user_friends.add(new User_friend("Pumpkin", "https://americanorchard.files.wordpress.com/2013/03/pumpkin-simple-image.jpg"));
-        user_friends.add(new User_friend("Banana", "http://saltmarshrunning.com/wp-content/uploads/2014/09/bananasf.jpg"));
-        user_friends.add(new User_friend("Strawberry", "http://www.adagio.com/images5/flavor_thumbnail/strawberry.jpg"));
-        user_friends.add(new User_friend("Watermelon", "http://www.wzdm.com/wp-content/uploads/2014/08/d0e5Watermelon2.jpg"));
-
-        lvFriends = (ListView) findViewById( R.id.listview);
-        adapterFriends = new User_friendListAdapterWithCache(this, user_friends);
-        lvFriends.setAdapter(adapterFriends);
-        lvFriends.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                //args2 is the listViews Selected index
-                // Send Invite
-
-                Log.d("MyApp", "User clicked on list item!");
-
+                Intent intent = new Intent(InviteContactsActivity.this, DispatchActivity.class);
+                startActivity(intent);
             }
         });
 
+
     }
 
-    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-    }
-
-    public void onScrollStateChanged(AbsListView view, int scrollState) {
-        switch (scrollState) {
-            case AbsListView.OnScrollListener.SCROLL_STATE_IDLE:
-                lvBusy = false;
-                adapterFriends.notifyDataSetChanged();
-                break;
-            case AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL:
-                lvBusy = true;
-                break;
-            case AbsListView.OnScrollListener.SCROLL_STATE_FLING:
-                lvBusy = true;
-                break;
-        }
-    }
-
-
-    public boolean isLvBusy(){
-        return lvBusy;
-    }
 
 }
